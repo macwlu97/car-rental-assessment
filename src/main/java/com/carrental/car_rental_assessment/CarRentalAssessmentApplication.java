1cbcaf151a915e5cdf0132b01aed516c22710242
macwlu97 <maciej.wnuklipinski@gmail.com>
2026-02-25 13:42:30 +0100

    initial commit

---
+++ src/main/java/com/carrental/car_rental_assessment/CarRentalAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.carrental.car_rental_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carrental/car_rental_assessment/CarRentalAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrental.car_rental_assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
